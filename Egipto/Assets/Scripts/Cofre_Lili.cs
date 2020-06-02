472e0ad13563dd456d6cebf5a40d567f5606c8bd
Liliana <lilisofiasc@gmail.com>
2020-06-01 19:25:09 -0500

    ScriptLili

---
+++ Egipto/Assets/Scripts/Cofre_Lili.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cofre_Lili : MonoBehaviour
+{
+    bool cajaAbriendose;
+    bool cajaCerrada;
+
+    public GameObject tapa;
+    public AudioSource sonido;
+
+    float inc;
+    float anguloActual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cajaAbriendose = false;
+        cajaCerrada = true;
+        inc = -0.8f;
+
+        anguloActual = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cajaAbriendose)
+        {
+            tapa.transform.Rotate(inc, 0, 0);
+            anguloActual += inc;
+
+            if (cajaCerrada)
+            {
+                inc = -0.8f;
+                if (anguloActual <= -100)
+                {
+                    cajaAbriendose = false;
+                    cajaCerrada = false;
+                }
+            }
+            else
+            {
+                inc = 0.8f;
+                if (anguloActual >= 0)
+                {
+                    cajaAbriendose = false;
+                    cajaCerrada = true;
+                }
+            }
+
+
+
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        cajaAbriendose = true;
+        sonido.Play();
+    }
+}
+
---
+++ Egipto/Assets/Scripts/Door_Lili.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Lili : MonoBehaviour
+{
+    bool puertaAbriendose;
+    bool puertaCerrada;
+
+    public GameObject tapapuerta;
+    public AudioSource sonido;
+
+    float inc;
+    float anguloActual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puertaAbriendose = false;
+        puertaCerrada = true;
+        inc = -0.6f;
+
+        anguloActual = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puertaAbriendose)
+        {
+            tapapuerta.transform.Rotate(0, inc, 0);
+            anguloActual += inc;
+
+            if (puertaCerrada)
+            {
+                inc = -0.6f;
+                if (anguloActual <= -160)
+                {
+                    puertaAbriendose = false;
+                    puertaCerrada = false;
+                }
+            }
+            else
+            {
+                inc = 0.6f;
+                if (anguloActual >= 0)
+                {
+                    puertaAbriendose = false;
+                    puertaCerrada = true;
+                }
+            }
+
+
+
+        }
+
+    }
+    private void OnMouseDown()
+    {
+        puertaAbriendose = true;
+        sonido.Play();
+    }
+}
