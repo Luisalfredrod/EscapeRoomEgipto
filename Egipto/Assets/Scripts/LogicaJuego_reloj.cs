1a577718f007798ef16fb16aed323bf970da524d
Liliana <lilisofiasc@gmail.com>
2020-06-02 00:19:00 -0500

    ScriptLiliEsUnElfoLibre

---
+++ Egipto/Assets/Scripts/LogicaJuego_reloj.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaJuego_reloj : MonoBehaviour
+{
+    public static bool corriendoTiempo = false;
+
+    private void OnMouseDown()
+    {
+        corriendoTiempo = true;
+    }
+        
+}
---
+++ Egipto/Assets/Scripts/MoverLuz.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class MoverLuz : MonoBehaviour
+{
+    float incX = 0.001f;
+
+    void FixedUpdate()
+    {
+        if (LogicaJuego_reloj.corriendoTiempo)
+        {
+            gameObject.transform.Translate(new Vector3(incX, 0, 0));
+            
+
+                if (gameObject.transform.position.x > -43)
+                    LogicaJuego_reloj.corriendoTiempo = false;
+            
+        }
+    }
+
+   
+    }
+
