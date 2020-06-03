6be4ab41197eb09c99719237254105e4d251f765
Ari León <ari.thomasway@gmail.com>
2020-06-03 13:34:21 -0500

    PruebaAri

---
+++ Egipto/Assets/PruebaAri/PruebaAriScripts/AriLogicaJuego.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AriLogicaJuego : MonoBehaviour
+{
+    public static bool[] estadoElementos = { false, false, false };
+
+    public static bool verificar()
+    {
+        if (estadoElementos[0] == true && estadoElementos[1] == true && estadoElementos[2] == true)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
---
+++ Egipto/Assets/PruebaAri/PruebaAriScripts/AriRotarElemento.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AriRotarElemento : MonoBehaviour
+{
+    bool rotando = false;
+    float incY = 1.0f;
+    float anguloAcumulado = 0;
+    public float anguloObjetivo;
+    public Light luz;
+    public int n;
+    public AudioSource sonido;
+   
+    private void FixedUpdate()
+    {
+        if (rotando)
+        {
+            gameObject.transform.Rotate(0, incY, 0);
+            anguloAcumulado += incY;
+            Debug.Log("AnguloAcumulado = " + anguloAcumulado);
+            if ((anguloAcumulado%360) > anguloObjetivo-3 && (anguloAcumulado%360) < anguloObjetivo+3)
+            {
+                luz.intensity = 6;
+                AriLogicaJuego.estadoElementos[n] = true;
+                if (AriLogicaJuego.verificar() == true)
+                {
+                    sonido.Play();
+
+
+
+                }
+            }
+            else {
+                luz.intensity = 0;
+                AriLogicaJuego.estadoElementos[n] = false;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        rotando = true;
+
+    }
+
+    private void OnMouseUp()
+    {
+        rotando = false;
+
+    }
+
+
+}
