6139a37b57a61c8d5df33c2f69e6f3d76308ffe7
Israel Galarza <A01638163@itesm.mx>
2020-06-03 22:51:16 -0500

    IsraFinal

---
+++ Egipto/Assets/Scripts/ActivaLuz.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ActivaLuz : MonoBehaviour
+{
+    public GameObject Luz1;
+    public GameObject Luz2;
+    public GameObject Luz3;
+    public GameObject Luz4;
+    public bool ActivaCaja;
+    public bool ActivaPiramide;
+    public bool ActivaTrono;
+    public bool ActivaJarron;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivaCaja = false;
+        ActivaPiramide = false;
+        ActivaTrono = false;
+        ActivaJarron = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ActivaPiramide == true && ActivaCaja == true && ActivaTrono == true && ActivaJarron == true)
+        {
+            Debug.Log("LuzPrendida");
+            StartCoroutine(IniciaPatron());
+        }
+    }
+
+    IEnumerator IniciaPatron()
+    {
+        yield return new WaitForSeconds(1f);
+        Luz1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz1.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz1.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        Luz2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz2.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        Luz3.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz3.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        Luz4.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Luz4.SetActive(false);
+        yield return new WaitForSeconds(3f);
+    }
+}
---
+++ Egipto/Assets/Scripts/LlegoCaja.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class LlegoCaja : MonoBehaviour
+{
+    AudioSource bien;
+    public GameObject objetoEsperado;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        bien = GameObject.Find("SonidoBien").GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+
+        if (other.name == objetoEsperado.name)
+        {
+            bien.Play();
+            Debug.Log("Se activo: " + other.name);
+            ActivaLuz activeC = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ActivaLuz>();
+            activeC.ActivaCaja = true;
+        }
+    }
+}
