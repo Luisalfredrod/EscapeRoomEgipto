e1746166692c912c7bca92cd93b8e5207f64a3d4
santiagovera <a01704428@itesm.mx>
2020-06-04 23:44:25 -0500

    ScriptsMEl

---
+++ Egipto/Assets/Scripts_Mel/TocarPiedras.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TocarPiedras : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("se dio clic sobre..." + gameObject.name);
+        GameLogic.agregarPiedra(gameObject.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
