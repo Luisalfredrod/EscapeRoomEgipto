46a2e1bf93d077b5bfadd1fe98071a5e8712dff5
ceciglez <ceci.gzroa@gmail.com>
2020-06-04 22:06:58 -0500

    CeciSave4

---
+++ Egipto/Assets/Sensor Luz/DetectarLuz.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class DetectarLuz : MonoBehaviour
+{
+    
+
+   public AudioSource sonido;
+
+
+    public GameObject objetoEsperado;
+    
+    public GameObject luz;
+    public VideoPlayer video;
+    
+   
+
+    private void Start()
+	{
+        
+        //luz = GameObject.Find("Point Light Ceci").GetComponent<GameObject>();
+        Debug.Log("start");
+
+       
+        
+    }
+
+
+     
+    private void OnTriggerEnter(Collider other)
+	{
+       
+        Debug.Log("Entró:" + other.name);
+        
+
+
+        if(other.name == objetoEsperado.name)
+        {
+            Debug.Log("ES UN CUBO");
+            other.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+            luz.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+
+
+            video.Play();
+         
+            sonido.Play();
+
+
+        }
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        sonido.Stop();
+    }
+
+
+}
+
+
+
---
+++ Egipto/Assets/Sensor Luz/playVideoFinal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class playVideoFinal : MonoBehaviour
+{
+    public GameObject objetoEsperado;
+
+    public VideoPlayer video;
+    // Start is called before the first frame update
+    void Start()
+    {
+        video.Pause();
+    }
+
+    private void OnMouseDown()
+    {
+        video.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Entró:" + other.name);
+
+
+
+        if (other.name == objetoEsperado.name)
+        {
+           video.Play();
+
+
+        }
+
+    }
+
+
+}
